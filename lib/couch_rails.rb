5773fb892ab0ac9bb6aee6d0a8809842671624a3
Felipe <felipe@felipe-laptop.(none)>
2010-03-10 20:29:31 -0300

    added abstract_class

--- lib/couch_rails.rb
+++ lib/couch_rails.rb
@@ -3,4 +3,5 @@ require 'active_model'
 
 module CouchRails
   autoload :Document, "couch_rails/document"
+  autoload :AbstractClass, "couch_rails/support/abstract_class"
 end
--- lib/couch_rails/document.rb
+++ lib/couch_rails/document.rb
@@ -2,9 +2,12 @@ require "couchrest"
 
 module CouchRails
   class Document < CouchRest::ExtendedDocument
-    include ActiveModel::Naming
+    extend ActiveModel::Naming
     include ActiveModel::Conversion
     include ActiveModel::Validations
-    
+    include AbstractClass
+
+    self.abstract_class = true
+
   end
 end
