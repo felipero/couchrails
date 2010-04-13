8bc79c58f9aee059cd63f8b1d5ae0142a8755cec
Felipe Rodrigues <felipero@gmail.com>
2010-04-13 10:53:01 -0300

    Testes to assert the right server uri

--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -3,14 +3,18 @@ require 'spec_helper'
 describe "CouchRails::Config" do
 
   before :each do
-    @localhost = "127.0.0.1:5984"
-    @cr = CouchRest.new(@localhost)
+    @couch_db_url = "http://127.0.0.1:5984"
+    @cr = CouchRest.new(@couch_db_url)
+  end
+
+  after :each do
+    @db.delete! if @db
   end
 
   it "should create a db based on model_name the model is not on the map" do
-    db = CouchRails::Config.db_for("modelos")
-    db.should be_kind_of CouchRest::Database
-    db.to_s.should == "#{@localhost}/modelos"
+    @db = CouchRails::Config.db_for("modelos")
+    @db.should be_kind_of CouchRest::Database
+    @db.to_s.should == "#{@couch_db_url}/modelos"
     @cr.databases.should include "modelos"
     dbs = CouchRails::Config.db_map
     dbs.should_not include :modelos
@@ -18,9 +22,10 @@ describe "CouchRails::Config" do
 
   it "should use the provided name" do
     CouchRails::Config.db_map = {:modelos => :sampledb}
-    db = CouchRails::Config.db_for("modelos")
-    db.should be_kind_of CouchRest::Database
-    db.to_s.should == "#{@localhost}/sampledb"
+    @db = CouchRails::Config.db_for("modelos")
+    @db.should be_kind_of CouchRest::Database
+    @db.to_s.should == "#{@couch_db_url}/sampledb"
+    @db.server.uri.should == @couch_db_url
     @cr.databases.should include "sampledb"
     dbs = CouchRails::Config.db_map
     dbs.should include :modelos
