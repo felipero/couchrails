a913e1a423b2e3161da52871820b15899d4575bd
Felipe Rodrigues <felipero@gmail.com>
2010-04-21 19:12:58 -0300

    Active model structure refactoring to prepare for more active model integration

--- lib/couchrails/core/document.rb
+++ lib/couchrails/core/document.rb
@@ -1,7 +1,7 @@
 module CouchRails
   class Document < CouchRest::ExtendedDocument
-    extend CouchRails::ActiveModel::ClassMethods
-    include CouchRails::ActiveModel::InstanceMethods
+    extend CouchRails::ActiveModel::Naming::ClassMethods
+    include CouchRails::ActiveModel::Naming::InstanceMethods
 
     def self.db(db_name = self.model_name.plural)
       self.use_database CouchRails::Config.db_for(db_name)
