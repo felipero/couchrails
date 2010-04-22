8be0e47082795cb626b2d1cb0e7bf4769744889f
Victor Hugo Germano <victorhg@gmail.com>
2010-04-22 01:09:19 +0100

    More Validations specs

--- spec/models.rb
+++ spec/models.rb
@@ -10,8 +10,12 @@ module Models
   end
 
   class DocWithValidations < CouchRails::Document
+    property :birth_year
     property :name
+
     validates_presence_of :name
+    validates_length_of :name, :maximum => 10, :minimum => 2
+    validates_format_of :birth_year, :with => /([0-9]+)/i, :allow_nil => true
   end
 end
 
--- spec/validations_spec.rb
+++ spec/validations_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
   describe "Testing Validations through ActiveModel" do
     before :each do
-      @model = Models::Doc.new
+      @model = Models::DocWithValidations.new
     end
 
     after :all do
@@ -9,8 +9,28 @@ require 'spec_helper'
       db.delete! if db
     end
 
-    it "should not allow saving a model registered with validations" do
-      @model = Models::DocWithValidations.new
+    it "should not save a model with empty value for attribute defined in validate_presence_of" do
+      @model.valid?.should be false
+    end
+
+    it "should be able to save model with attr field correctly" do
+      @model.name= "NAME"
+      @model.valid?.should be true
+    end
+
+    it "should not save with attribute more than 10 as defined in validate_length_of" do
+      @model.name = "Name with more than 10 characters"
+      @model.valid?.should be false
+    end
+
+    it "should not save model with less than 2 chars" do
+      @model.name = "A"
+      @model.valid?.should be false
+    end
+
+    it "should only allow numbers on age attr"  do
+      @model.name = "NAME"
+      @model.birth_year = "A"
       @model.valid?.should be false
     end
   end
