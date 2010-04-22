e34969595c1baedf32c8a1d140cd054772fbab00
Felipe Rodrigues <felipero@gmail.com>
2010-04-21 21:10:31 -0300

    moved to our own validations module

--- lib/couchrails/active_model/validations.rb
+++ lib/couchrails/active_model/validations.rb
@@ -1,8 +1,10 @@
-
 module CouchRails
   module Validations
-       
+    def self.included(subclass)
+      subclass.instance_eval do
+        include ActiveModel::Validations
+      end
+    end
+
   end
-
-
 end
--- lib/couchrails/core/document.rb
+++ lib/couchrails/core/document.rb
@@ -2,8 +2,7 @@ module CouchRails
   class Document < CouchRest::ExtendedDocument 
     extend CouchRails::Naming::ClassMethods
     include CouchRails::Naming::InstanceMethods
-    include ActiveModel::Validations
-
+    include CouchRails::Validations
 
     def self.db(db_name = self.model_name.plural)
       self.use_database CouchRails::Config.db_for(db_name)
