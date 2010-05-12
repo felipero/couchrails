00e98a656b449a0377672c3a766273416e6c4496
Felipe Rodrigues <felipero@gmail.com>
2010-05-12 16:14:45 -0300

    Improved the simple example to show queries

--- examples/post_example.rb
+++ examples/post_example.rb
@@ -1,4 +1,4 @@
-require 'couchrails'
+require 'couch_rails'
 
 class Post < CouchRails::Document
 	property :title
@@ -14,6 +14,15 @@ post.title = "Demo Post"
 post.content =  "This is just an example of using CouchRails. Still under development... stay put"
 
 puts "Saved!" if post.save!
+
+post_recovered = Post.find(post.id) # Post.get(post.id)
+
+puts "Take a look at how it comes back from the DB"
+puts post_recovered
+
+puts "Let's list all posts on the DB"
+posts = Post.all
+puts posts
 
 puts "Please check your couchDB installation, you might find one record"
 
